8f4f01d44822c2388182ca4d938ff94497e1289a
Miguel Mendes <miguel.mendes.o@gmail.com>
2024-02-18 15:45:47 +0100

    Refactor dropdown menu and update top navigation

--- src/components/model-menu.tsx
+++ src/components/model-menu.tsx
@@ -2,47 +2,53 @@
 
 import * as React from "react"
 import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
-
+import { ChevronDownIcon } from "@radix-ui/react-icons"
 import { Button } from "@/components/ui/button"
 import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
 type Checked = DropdownMenuCheckboxItemProps["checked"]
 
 export function ModelMenu() {
-  const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true)
-  const [showPanel, setShowPanel] = React.useState<Checked>(false)
+    const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true)
+    const [showPanel, setShowPanel] = React.useState<Checked>(false)
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline">Choose model</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>Models</DropdownMenuLabel>
-        <DropdownMenuSeparator />
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                    Choose model
+                    <ChevronDownIcon
+                        className="ml-2 h-4 w-4 text-muted-foreground"
+                        aria-hidden="true"
+                    />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>Models</DropdownMenuLabel>
+                <DropdownMenuSeparator />
 
-        <DropdownMenuCheckboxItem
-          checked={showStatusBar}
-          onCheckedChange={setShowStatusBar}
-        >
-          Mistral
-        </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                    checked={showStatusBar}
+                    onCheckedChange={setShowStatusBar}
+                >
+                    Mistral
+                </DropdownMenuCheckboxItem>
 
-        <DropdownMenuCheckboxItem
-          checked={showPanel}
-          onCheckedChange={setShowPanel}
-        >
-          Llama2
-        </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                    checked={showPanel}
+                    onCheckedChange={setShowPanel}
+                >
+                    Llama2
+                </DropdownMenuCheckboxItem>
 
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
 }
--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -2,9 +2,8 @@
 
 import * as React from "react"
 import Link from "next/link"
-
 import { cn } from "@/lib/utils"
-//import { Icons } from "@/components/icons"
+
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -59,27 +58,11 @@ export const TopNav: React.FC = () => {
     <header className="container">
       <NavigationMenu>
         <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
 
-                <ListItem href="/docs" title="Introduction">
-                  Re-usable components built using Radix UI and Tailwind CSS.
-                </ListItem>
-                <ListItem href="/docs/installation" title="Installation">
-                  How to install dependencies and structure your app.
-                </ListItem>
-                <ListItem href="/docs/primitives/typography" title="Typography">
-                  Styles for headings, paragraphs, lists...etc
-                </ListItem>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuTrigger>Components</NavigationMenuTrigger>
             <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                 {components.map((component) => (
                   <ListItem
                     key={component.title}
@@ -92,16 +75,19 @@ export const TopNav: React.FC = () => {
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
+
           <NavigationMenuItem>
-            <Link href="/docs" legacyBehavior passHref>
+            <Link href="/dashboard" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Documentation
+                Dashboard
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
+
           <NavigationMenuItem>
             <ModeToggle />
           </NavigationMenuItem>
+          
         </NavigationMenuList>
       </NavigationMenu>
     </header>
