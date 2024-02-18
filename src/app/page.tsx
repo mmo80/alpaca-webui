0a36535dc0d760b9880789e02194ad1d79a76e4d
Miguel Mendes <miguel.mendes.o@gmail.com>
2024-02-18 15:45:59 +0100

    Refactor chat functionality and add input field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { ChatBubble } from "@/components/chat-bubble";
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { ModelMenu } from "@/components/model-menu";
+import { useState } from 'react';
 
 type ChatMessage = {
   human: boolean;
   message: string;
 }
 
-const chat: ChatMessage[] = [
+const chatsMock: ChatMessage[] = [
   {
     human: false,
     message: "Hi, how can I help you today?",
@@ -37,6 +31,17 @@ const chat: ChatMessage[] = [
 ];
 
 export default function Home() {
+  const [chat, setChat] = useState<string>('');
+  const [chats, setChats] = useState<ChatMessage[]>([]);
+
+  const handleSend = () => {
+    setChats([
+      ...chats,
+      { message: chat, human: true }
+    ]);
+    setChat('');
+  }
+
   return (
     <>
       <main className="bg-background container">
@@ -45,21 +50,15 @@ export default function Home() {
         </div>
         <div className="py-5 pt-4">
           <div className="space-y-4">
-            {chat.map((message, index) => (
+            {chats.map((message, index) => (
               <ChatBubble human={message.human} message={message.message} key={index} />
             ))}
-            {/* 
-            <div className="flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm bg-muted">Hi, how can I help you today?</div>
-            <div className="flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm ml-auto bg-primary text-primary-foreground">Hey, Im having trouble with my account.</div>
-            <div className="flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm bg-muted">What seems to be the problem?</div>
-            <div className="flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm ml-auto bg-primary text-primary-foreground">I cant log in.</div>
-            */}
           </div>
         </div>
 
         <div className="flex w-full max-w-sm items-center space-x-2">
-          <Input type="email" placeholder="Type your message..." />
-          <Button type="submit">Send</Button>
+          <Input type="text" value={chat} onChange={e => setChat(e.target.value)} placeholder="Type your message..." />
+          <Button onClick={handleSend}>Send</Button>
         </div>
 
       </main>
