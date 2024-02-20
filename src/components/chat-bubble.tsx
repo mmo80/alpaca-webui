f5daf73420c9cb8a37be216027ec1d486e98549f
Miguel Mendes <miguel.mendes.o@gmail.com>
2024-02-20 19:17:07 +0100

    feat ✨: Fix whitespace wrapping around chat message content overflowing container
    The changes made to the ChatBubble component aim to fix 🐛 an issue with the whitespace wrapping around the message content, which was causing

--- src/components/chat-bubble.tsx
+++ src/components/chat-bubble.tsx
@@ -5,8 +5,8 @@ import { ChatMessage, ChatRole } from "@/lib/types";
 
 export const ChatBubble: React.FC<ChatMessage> = ({ role, content }) => {
     return (
-        <div className={`flex w-max max-w-[100%] flex-col gap-2 rounded-lg px-3 py-2 text-sm ${role == ChatRole.USER ? 'whitespace-pre-wrap bg-primary text-primary-foreground' : 'bg-muted'}`}>
+        <pre className={`flex w-max max-w-[100%] flex-col gap-2 rounded-lg px-3 py-2 text-wrap text-sm ${role == ChatRole.USER ? 'whitespace-pre-wrap bg-primary text-primary-foreground' : 'bg-zinc-900'}`}>
             {content}
-        </div>
+        </pre>
     );
 }
