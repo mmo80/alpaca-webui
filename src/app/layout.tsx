4325f9c573cef0813c1c24b4ca07f3754c6365b4
Miguel Mendes <miguel.mendes.o@gmail.com>
2024-03-01 16:02:02 +0100

    removed unused files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,22 @@
-import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { TopNav } from "@/components/top-nav";
-import { Provider } from "@/app/provider";
-
-// const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next';
+import './globals.css';
+import { TopNav } from '@/components/top-nav';
+import { Provider } from '@/app/provider';
 
 export const metadata: Metadata = {
-  title: "Alpaca for Ollama",
-  description: "Simple chat UI for Ollama",
+  title: 'Alpaca for Ollama',
+  description: 'Simple chat UI for Ollama',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <body className="min-h-screen bg-background font-sans antialiased">
-          <div className="container flex flex-col h-screen overflow-hidden">
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <TopNav />
-              <Provider>{children}</Provider>
-            </ThemeProvider>
-          </div>
-        </body>
-      </html>
-    </>
+    <html lang="en" suppressHydrationWarning>
+      <body className="dark min-h-screen bg-background font-sans antialiased">
+        <div className="container flex flex-col h-screen overflow-hidden">
+          <TopNav />
+          <Provider>{children}</Provider>
+        </div>
+      </body>
+    </html>
   );
 }
