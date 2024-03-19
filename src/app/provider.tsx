748502eae55717dfd15929ddb52fbd4edfbc08f5
Miguel Mendes <miguel.mendes.o@gmail.com>
2024-03-19 11:28:47 +0100

    prettier

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,8 +1,8 @@
-"use client";
-import React, { useState } from "react";
-import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+'use client';
+import React, { useState } from 'react';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function Provider({ children }: any) {
   const [client] = useState(new QueryClient());
