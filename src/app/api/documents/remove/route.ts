d4d74a32d5c641c778449e5f83261291026fe430
Miguel Mendes <miguel.mendes.o@gmail.com>
2024-04-12 01:38:31 +0200

    fixed remove file if not embedded

--- src/app/api/documents/remove/route.ts
+++ src/app/api/documents/remove/route.ts
@@ -10,14 +10,20 @@ export async function POST(request: Request) {
   const body = await request.json();
 
   const documentId = body.documentId;
-  const dbResult = await db.select({ filename: files.filename }).from(files).where(eq(files.id, documentId));
+  const dbResult = await db
+    .select({ filename: files.filename, embedded: files.isEmbedded })
+    .from(files)
+    .where(eq(files.id, documentId));
   if (dbResult.length > 0) {
     const filename = dbResult[0].filename;
+    const isEmbedded = dbResult[0].embedded;
     const deleteResult = await db.delete(files).where(eq(files.id, documentId)).returning({ id: files.id });
     if (deleteResult.length === 1 && deleteResult[0].id == documentId) {
-      const vResult = await deleteFromVectorDatabase(filename);
-      if (!vResult) {
-        throw new Error(`Could not delete file ${documentId} from vector database.`);
+      if (isEmbedded) {
+        const vResult = await deleteFromVectorDatabase(filename);
+        if (!vResult) {
+          throw new Error(`Could not delete file ${documentId} from vector database.`);
+        }
       }
       await removeFileFromDisk(filename);
     } else {
